2b40cd8b5eea9260c6b94b44303981214e35695c
Maris-Pidiks <maris@divikiwi.lv>
2025-10-31 11:09:08 +0200

    Add auth instead index tab

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,58 +1,86 @@
-import { Link } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
-import { Body, Title } from '../../globalStyles/typography';
-import { COLORS, TYPOGRAPHY, SPACING } from '../../globalStyles/theme';
-import { flexPatterns } from '../../globalStyles/layoutStyles';
-import { useTheme } from '../../globalStyles/ThemeContext';
-import { ThemeToggle } from '../../components/ThemeToggle';
+import 'react-native-url-polyfill/auto';
+import { useState, useEffect } from 'react';
+import { supabase } from '../../lib/supabase';
+import Auth from '../../components/Auth';
+import { View, Text } from 'react-native';
+import { Session } from '@supabase/supabase-js';
 
-export default function Index() {
-  const { theme } = useTheme();
+export default function App() {
+  const [session, setSession] = useState<Session | null>(null);
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: theme.colors.background,
-      justifyContent: 'center',
-      alignItems: 'center',
-    },
-    title: {
-      color: theme.colors.text,
-      fontSize: TYPOGRAPHY.xl,
-      fontWeight: 'bold',
-      marginBottom: SPACING.md,
-    },
-    text: {
-      color: theme.colors.text,
-      marginBottom: SPACING.md,
-    },
-    button: {
-      marginTop: SPACING.md,
-      padding: SPACING.md,
-      backgroundColor: theme.colors.primary,
-      borderRadius: 8,
-      color: '#fff',
-      textAlign: 'center',
-      width: 200,
-    },
-  });
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
 
   return (
-    <View style={[styles.container, flexPatterns.center]}>
-      <Title style={styles.title}>HOME SCREEN</Title>
-      <Body style={styles.text}>Home screen</Body>
-
-      <Link href="/personal" style={styles.button}>
-        Go to Contacts screen
-      </Link>
-      <Link href="/statistic" style={styles.button}>
-        Go to Statistic screen
-      </Link>
-      <Link href="/admin" style={[styles.button, { marginBottom: SPACING.md }]}>
-        Go to Admin screen
-      </Link>
-
-      <ThemeToggle />
+    <View>
+      <Auth />
+      {session && session.user && <Text>{session.user.id}</Text>}
     </View>
   );
 }
+
+// import { Link } from 'expo-router';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { Body, Title } from '../../globalStyles/typography';
+// import { COLORS, TYPOGRAPHY, SPACING } from '../../globalStyles/theme';
+// import { flexPatterns } from '../../globalStyles/layoutStyles';
+// import { useTheme } from '../../globalStyles/ThemeContext';
+// import { ThemeToggle } from '../../components/ThemeToggle';
+
+// export default function Index() {
+//   const { theme } = useTheme();
+
+//   const styles = StyleSheet.create({
+//     container: {
+//       flex: 1,
+//       backgroundColor: theme.colors.background,
+//       justifyContent: 'center',
+//       alignItems: 'center',
+//     },
+//     title: {
+//       color: theme.colors.text,
+//       fontSize: TYPOGRAPHY.xl,
+//       fontWeight: 'bold',
+//       marginBottom: SPACING.md,
+//     },
+//     text: {
+//       color: theme.colors.text,
+//       marginBottom: SPACING.md,
+//     },
+//     button: {
+//       marginTop: SPACING.md,
+//       padding: SPACING.md,
+//       backgroundColor: theme.colors.primary,
+//       borderRadius: 8,
+//       color: '#fff',
+//       textAlign: 'center',
+//       width: 200,
+//     },
+//   });
+
+//   return (
+//     <View style={[styles.container, flexPatterns.center]}>
+//       <Title style={styles.title}>HOME SCREEN</Title>
+//       <Body style={styles.text}>Home screen</Body>
+
+//       <Link href="/personal" style={styles.button}>
+//         Go to Contacts screen
+//       </Link>
+//       <Link href="/statistic" style={styles.button}>
+//         Go to Statistic screen
+//       </Link>
+//       <Link href="/admin" style={[styles.button, { marginBottom: SPACING.md }]}>
+//         Go to Admin screen
+//       </Link>
+
+//       <ThemeToggle />
+//     </View>
+//   );
+// }
